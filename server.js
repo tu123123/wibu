d180f4570bb3749786c8471960e7d222e78a2e1c
tu <tu1231230@gmail.com>
2022-03-24 17:46:36 +0700

    json3

--- server.js
+++ server.js
@@ -2,9 +2,9 @@ const jsonServer = require("json-server");
 const server = jsonServer.create();
 const router=jsonServer.router("/src/database/db.json");
 const middlewares=jsonServer.defaults({
-    static:"./build",
+    static:"/build",
 });
-const port=process.env.port||8000;
+const port=process.env.PORT||8000;
 server.use(middlewares);
 server.use(
     jsonServer.rewriter({
